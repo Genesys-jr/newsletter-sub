7a8eab3f2b1ed79f172e4223cb18c905c094757c
Amon Daniel <amon.daniel.91@gmail.com>
2023-07-24 14:25:51 +0100

    moibile success done

--- src/app/submission/[email]/page.tsx
+++ src/app/submission/[email]/page.tsx
@@ -1,8 +1,7 @@
-"use client";
-import { useRouter } from "next/navigation";
-
-import Image from "next/image";
-import success from "../../../../public/success.svg";
+'use client'
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import success from '../../../../public/success.svg';
 
 interface Params {
   params: { email: string };
@@ -16,25 +15,21 @@ export default function Page({ params }: Params) {
   };
 
   return (
-    <div className="flex items-center justify-center h-screen bg-slate-600 md:bg-slate-700">
-      <div className="bg-white md:w-full md:max-w-md p-10 rounded-lg">
+    <div className="min-h-screen bg-white md:bg-slate-700 flex flex-col justify-center items-center">
+      <div className=" bg-white w-full max-w-md p-10 rounded-lg">
         <div className="text-center">
-          <Image
-            src={success}
-            alt=""
-            className="object-contain my-10 mx-auto"
-          />
+          <Image src={success} alt="" className="object-contain my-10 mx-auto" />
           <h1 className="text-3xl font-bold mb-10">Thanks for subscribing!</h1>
           <p className="mb-auto">
             A confirmation email has been sent to
-            <span className="font-bold"> {decodedEmail}</span>. Please open it
-            and click the button inside to confirm your subscription.
+            <span className="font-bold"> {decodedEmail}</span>. Please open it and click the
+            button inside to confirm your subscription.
           </p>
         </div>
 
-        <div>
+        <div className="mt-8 md:mt-12 md:mb-0 md:pb-8">
           <button
-            className="bg-slate-800 text-white w-full py-5 rounded-lg text-xl mt-8 bg-gradient-to-r  hover:from-pink-500 hover:to-yellow-500"
+            className="bg-slate-800 text-white w-full py-5 rounded-lg text-xl bg-gradient-to-r hover:from-pink-500 hover:to-yellow-500"
             onClick={handleDismiss}
           >
             Dismiss message
